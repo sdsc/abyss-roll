56b37b2a6192fd53f5f5c6f9f2e3481251e9b6f0
Jerry Greenberg <jpg@comet-devel.local>
2015-03-10 10:32:15 -0700

    added commit to roll name

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = abyss
-RELEASE  = 0
-COLOR    = orange
+ROLLNAME        = abyss
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT = $(PWD)
+REDHAT.ROOT     = $(PWD)
