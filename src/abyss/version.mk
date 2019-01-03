126f64a7643a83723a75f226086eba6075f5a819
Jerry Greenberg <jpg@sdsc.edu>
2019-01-03 15:05:40 -0800

    APS-859 re-added sparsehash zip file to binary_hashes and updated version.mk version number

--- src/abyss/version.mk
+++ src/abyss/version.mk
@@ -9,7 +9,7 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-abyss
-VERSION        = 2.1.1
+VERSION        = 2.1.5
 RELEASE        = 0
 PKGROOT        = /opt/abyss
 
